0205fffbf663d09b8129de7eef0742c1d358fbe9
Abhiramvadla12 <abhiramvadla61@gmail.com>
2024-12-20 09:37:32 +0530

    version 2

--- content/header_data1.js
+++ content/header_data1.js
@@ -3,5 +3,10 @@ export const menu1 = [
 {
     id:"1",
     menuItem:"Username : Abhiram"
+},
+{
+    id:"2",
+    menuItem:"HOme",
+    path:"../index.html"
 }
 ]
--- content/header_data2.js
+++ content/header_data2.js
@@ -3,5 +3,10 @@ export const menu2 = [
     {
         id:"1",
         menuItem:"Username : Abhiram"
+    },
+    {
+        id:"1",
+        menuItem:"HOme",
+        path:"../index.html"
     }
     ]
